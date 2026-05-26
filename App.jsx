389597139c8e9aff9474efae1fc70a30a6dbe342
TARUN V <tv4002328@gmail.com>
2026-05-26 19:15:13 +0530

    Update and rename README.md to App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+# jsx.app
+import React from 'react';
+import './App.css';
+function App() {
+ return (
+ <div className="landing-page">
+ <h1>Paradise Nursery</h1>
+ <button>Get Started</button>
+ </div>
+ );
+  }
+export default App;
